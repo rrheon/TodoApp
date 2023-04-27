d4072f571bdf3e371e7189b9d67c5f7f6c65ffe4
최용헌 <yongheon0806@gmail.com>
2023-04-27 14:37:46 +0900

    customcell 구현

--- ToDoApp/Controllers/CustomCell.swift
+++ ToDoApp/Controllers/CustomCell.swift
@@ -1,23 +1,84 @@
-//
-//  CustomCell.swift
-//  ToDoApp
-//
-//  Created by 최용헌 on 2023/04/27.
-//
-
 import UIKit
+import SnapKit
 
-class CustomCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+final class CustomCell: UITableViewCell {
+    // 해당 셀의 identifier 지정
+    static let cellId = "CustomCell"
+    
+    // MARK: - cell 구성
+    let priorityLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        label.textColor = .black
+        label.textAlignment = .center
+        label.layer.cornerRadius = 8
+        label.layer.masksToBounds = true
+        return label
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        return label
+    }()
+    
+    let deadlineLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = UIColor.darkGray
+        return label
+    }()
+    
+    let completedImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "checkmark.circle")
+        imageView.tintColor = UIColor.systemGreen
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        [
+            priorityLabel,
+            titleLabel,
+            deadlineLabel,
+            completedImageView,
+        ].forEach {
+            self.addSubview($0)
+        }
+        
+        makeUI()
+     
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func makeUI(){
+        priorityLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.leading.equalToSuperview().offset(16)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(priorityLabel.snp.bottom).offset(8)
+            make.leading.equalTo(priorityLabel)
+            make.trailing.equalTo(completedImageView.snp.leading).offset(-16)
+        }
+        
+        deadlineLabel.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-8)
+            make.leading.equalTo(priorityLabel)
+        }
+        
+        completedImageView.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().offset(-16)
+            make.centerY.equalTo(priorityLabel)
+            make.width.height.equalTo(30)
+        }
     }
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
 }
