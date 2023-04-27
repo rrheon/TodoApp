95be1c186ffc7a650c55b6612c0a2d7bbb4c2269
최용헌 <yongheon0806@gmail.com>
2023-04-27 14:36:28 +0900

    mainUI

--- ToDoApp/Controllers/MainViewController.swift
+++ ToDoApp/Controllers/MainViewController.swift
@@ -6,46 +6,89 @@
 //
 import UIKit
 
-class MainViewController: UIViewController {
 
+
+final class MainViewController: UIViewController, AddItemDelegate {
+    // TableView에 사용될 데이터 모델
+    var items = [Info]()
+    
+    // MARK: - tableView 설정
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.rowHeight = 120
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        self.title = "홈"
+        self.title = "ToDoList"
         self.view.backgroundColor = .white
+    
+  
+        // TableView 설정
+        tableView.register(CustomCell.self, forCellReuseIdentifier: CustomCell.cellId)
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
         
-        // 네비게이션 아이템 추가
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.fill"), style: .plain, target: self, action: #selector(goToProfileVC))
+        // TableView Layout 설정
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading)
+            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing)
+        }
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "message.fill"), style: .plain, target: self, action: #selector(goToMessageVC))
+        // MARK: - navigationbar 설정
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Log out", style: .plain, target: self, action: #selector(goToLoginVC))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(addTableView))
         
-                
+      
+        // 데이터 모델 초기화
+        items = [
+            Info(title:"1",priority: "High", memo: "item 1", date: "2023-04-28"),
+            Info(title:"2",priority: "Medium", memo: "item 2", date: "2023-04-29"),
+            Info(title:"3",priority: "Low", memo: "item 3", date: "2023-04-30")
+        ]
+    }
+    // MARK: - 메서드 설정
+    @objc fileprivate func goToLoginVC() {
+        let loginVC = LoginViewController()
+        self.navigationController?.pushViewController(loginVC, animated: true)
+        loginVC.modalPresentationStyle = .fullScreen
     }
     
-    
-    @objc fileprivate func goToProfileVC(){
-        print("MainViewController - goToProfileVC() 호출됨")
-        
-        let profileVC = LoginViewController()
-        self.navigationController?.pushViewController(profileVC, animated: true)
-        profileVC.modalPresentationStyle = .fullScreen
-//        self.present(profileVC, animated: true)
-        
-    }
-    
-    @objc fileprivate func goToMessageVC(){
-        print("MainViewController - goToMessageVC() 호출됨")
-        
-//        let messageVC = MessageViewController()
-//        self.navigationController?.pushViewController(messageVC, animated: true)
+    @objc fileprivate func addTableView() {
+        let addItemVC = AddItemViewController()
+        self.navigationController?.pushViewController(addItemVC, animated: true)
+        addItemVC.modalPresentationStyle = .fullScreen
+        addItemVC.delegate = self
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.fill"), style: .plain, target: self, action: #selector(goToProfileVC))
+    func addItem(item: Info) {
+        items.append(item)
+        tableView.reloadData()
+    }
+}
 
+// MARK: - tableView custom
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.cellId, for: indexPath) as! CustomCell
+        
+        // CustomCell에 데이터 전달
+        cell.titleLabel.text = items[indexPath.row].title
+        cell.deadlineLabel.text = items[indexPath.row].date
+        cell.priorityLabel.text = items[indexPath.row].priority
+    
+        return cell
+        
     }
 
 }
-
--- ToDoApp/Models/DataManager.swift
+++ ToDoApp/Models/DataManager.swift
@@ -5,4 +5,19 @@
 //  Created by 최용헌 on 2023/04/12.
 //
 
-import Foundation
+import UIKit
+
+struct Info {
+    let title: String
+    let priority: String?
+    let memo: String
+    let date: String
+    var isCompleted: Bool = false
+    
+    init(title: String, priority: String?, memo: String, date: String = "") {
+        self.title = title
+        self.priority = priority
+        self.memo = memo
+        self.date = date
+    }
+}
